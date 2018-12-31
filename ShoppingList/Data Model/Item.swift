70d720d6022c1f06c91de9bc5ffa45c30affe846
Nick Natali <nickolasnatali@gmail.com>
2018-12-31 11:38:58 -0800

    converted to mvc design pattern.

---
+++ ShoppingList/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  ShoppingList
+//
+//  Created by Nick Natali on 12/31/18.
+//  Copyright © 2018 Nick Natali. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
